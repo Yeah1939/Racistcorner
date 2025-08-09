7b3c74a3e3789220c86359dedcba24c5437e5fcb
Yeah1939 <cocacolatwink@gmail.com>
2025-08-09 13:28:10 +0300

    Create sits.py

---
+++ sits.py
@@ -0,0 +1,16 @@
+# напиши модуль для підрахунку кількості присідань
+from kivy.uix.label import Label
+from kivy.clock import Clock
+class Sits(Label):
+    
+    def __init__(self, total, **kwargs):
+        self.total = total
+        self.current = 0
+        my_text = f"Залишилось присідань{self.total}"
+        super().__init__(text=my_text,**kwargs)
+
+    def next(self, *args):
+        self.current +=1
+        tmp = self.total - self.current
+        my_text = f"Залишилось присідань{tmp}"
+        self.text = my_text
